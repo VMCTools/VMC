7cf696e609a0dd9b41ea3a4eb577a8a2bf66888d
CuongVM <cuongvm@wildthunder.org>
2022-11-18 17:31:14 +0700

    update Move

---
+++ VMC/Ingame/Move/MoveWithJoyStickPOV.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI.Extensions;
+using VMC.Ultilities;
+
+namespace VMC.Ingame.Move
+{
+    public class MoveWithJoyStickPOV : MoveWithJoyStick
+    {
+        protected override void Joystick_OnDragAction(Vector2 delta)
+        {
+            direction = transform.forward * delta.y + transform.right * delta.x;
+        }
+        protected override void GetInputKeyboard()
+        {
+            if (isDragging) return;
+            if (!getInputFromKeyboard) return;
+            direction = transform.forward * Input.GetAxis("Vertical") + transform.right * Input.GetAxis("Horizontal");
+            isGetingInputFromKeyboard = direction.x != 0 || direction.z != 0;
+        }
+    }
+}
