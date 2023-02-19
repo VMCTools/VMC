9f0e3412eb321a449418e6157a71077454bf160b
CườngVM - HOME <cuongvm2307@gmail.com>
2023-02-19 22:36:54 +0700

    Update FloatExtensions.cs

--- VMC/Ultilities/FloatExtensions.cs
+++ VMC/Ultilities/FloatExtensions.cs
@@ -1,22 +1,20 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+using System;
 
 namespace VMC.Ultilities
 {
     public static class FloatExtensions
     {
-        //public static string ShowTimeDHMS(this float time)
-        //{
-        //    return $"{timeSpan.Days.ToString("00")}:{timeSpan.Hours.ToString("00")}:{timeSpan.Minutes.ToString("00")}:{timeSpan.Seconds.ToString("00")}";
-        //}
-        //public static string ShowTimeHMS(this float time)
-        //{
-        //    return $"{timeSpan.Hours.ToString("00")}:{timeSpan.Minutes.ToString("00")}:{timeSpan.Seconds.ToString("00")}";
-        //}
+        public static string ShowTimeDHMS(this float time)
+        {
+            return TimeSpan.FromSeconds(time).ToString(@"dd\.hh\:mm\:ss");
+        }
+        public static string ShowTimeHMS(this float time)
+        {
+            return TimeSpan.FromSeconds(time).ToString(@"\ hh\:mm\:ss");
+        }
         public static string ShowTimeMS(this float time)
         {
-            return $"{((int)time / 60).ToString("00")}:{((int)time % 60).ToString("00")}";
+            return TimeSpan.FromSeconds(time).ToString(@"mm\:ss");
         }
     }
 }
